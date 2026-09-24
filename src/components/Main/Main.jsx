2eced09a34a001cb0bb11e50c5f0aff0782636ac
HELENA OLIVEIRA <helena_oliveira152@estudante.sesisenai.org.br>
2026-09-24 16:57:01 -0300

    segundo commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,23 @@
+import './Main.css'
+
+function Main(){
+    return(
+        <main className='main'>
+            <section className='hero'>
+                <h1>Criamos sites que funcionam</h1>
+                <p>Layouts responsivos, rápidos e acessíveis para o seu negócio crescer na web.</p>
+
+                <div className='hero-buttons'>
+                    <a href="#orcamento" className='btn-primary'>Peça um orçamento</a>
+                    <a href="#potifolio" className='btn-segundary'>Ver portifólio</a>
+                </div>
+            </section>
+
+            <section className='servicos'>
+
+            </section>
+        </main>
+    )
+}
+
+export default Main
